3ad15b45974ebbefec3566f8b61a181883533200
AMIRKIAYI <amirkiayi516@gmail.com>
2024-05-24 07:45:42 +0300

    index.js file update

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+//Created a container to store our created h1 element
+const newHeader = document.createElement("h1");
+//Give our h1 element an id called victory
+newHeader.id = "victory"
+//inserted the text in the h1 element
+newHeader.textContent = "AMIR OSMAN is the champion"
